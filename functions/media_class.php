a901f68f0c27b57297cd715f627e71555cda1c6e
Dominic <dom@percolatestudio.com>
2014-04-05 13:53:30 -0700

    Single: ensure that images generated by wordpress 'add media' get appropriate class
    Because they are wrapped in links they end up getting and underline.
    This commit adds a class that we then target to prevent such an
    occurence.

---
+++ functions/media_class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Attach a class to linked images' parent anchors
+ * e.g. a img => a.img img
+ */
+function give_linked_images_class($html, $id, $caption, $title, $align, $url, $size, $alt = '' ){
+  $classes = 'img'; // separated by spaces, e.g. 'img image-link'
+
+  // check if there are already classes assigned to the anchor
+  if ( preg_match('/<a.*? class=".*?">/', $html) ) {
+    $html = preg_replace('/(<a.*? class=".*?)(".*?>)/', '$1 ' . $classes . '$2', $html);
+  } else {
+    $html = preg_replace('/(<a.*?)>/', '$1 class="' . $classes . '" >', $html);
+  }
+  return $html;
+}
+add_filter('image_send_to_editor','give_linked_images_class',10,8);
+?>
