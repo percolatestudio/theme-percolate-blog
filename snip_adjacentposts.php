925f65a0e0cbc82f8cfcd55c0ef52f4bce3ba5a2
Dominic <dom@percolatestudio.com>
2014-04-05 14:19:05 -0700

    Single: adding bg texture to adjacent posts

--- snip_adjacentposts.php
+++ snip_adjacentposts.php
@@ -28,7 +28,7 @@
   ?>
   <?php if(get_adjacent_post(false, '', true)) {?>
   <div class="post <?php if(has_post_thumbnail($previous_post->ID)) {echo 'has-image';}?>"
-    style='background-image: url("<?php echo $previous_image[0] ?>");'>
+    style='background-image: url("<?php echo $previous_image[0] ?>");' title="<?php echo $previous_title; ?>">
     <div class="post-content">
       <h3 class="title">
         <a class="inverse" href="<?php echo $previous_link; ?>"><?php echo $previous_title; ?></a>
@@ -69,7 +69,7 @@
 
   <?php if(get_adjacent_post(false, '', false)) {?>
   <div class="post <?php if(has_post_thumbnail($next_post->ID)) {echo 'has-image';}?>"
-    style='background-image: url("<?php echo $next_image[0] ?>");'>
+    style='background-image: url("<?php echo $next_image[0] ?>");' title="<?php echo $next_title; ?>">
 
     <div class="post-content">
       <h3 class="title">
