1d64c0400c26a3ec7e5425de9dd15d8e82a11566
Dominic <dom@percolatestudio.com>
2014-04-07 16:29:16 -0700

    Single: finalizing metadata layout

--- snip_metadata.php
+++ snip_metadata.php
@@ -1,12 +1,13 @@
 <div class="metadata">
-  <!-- <a href="<?php the_permalink(); ?>#comments"><span class="icon-comment"></span> <?php comments_number('0', '1', '%'); ?></a> -->
+  <div class="definition"><span>Published:</span> <?php the_time('M j, Y'); ?></div>
+	<div class="definition"><span>Categories:</span> <?php the_category(', '); ?></div>
+	<div class="definition"><?php the_tags('<span>Tags:</span> ', ', ', ''); ?> </div>
+
+	<!-- <a href="<?php the_permalink(); ?>#comments"><span class="icon-comment"></span> <?php comments_number('0', '1', '%'); ?></a> -->
 
   <a class="link-social" href="http://twitter.com/home?status=Currently reading <?php the_permalink(); ?>" title="Tweet this article" target="_blank"><span class="icon-twitter"></span></a>
 
   <!-- More info on FB share -->
   <a class="link-social" href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>" onclick="return fbs_click()" target="_blank"><span class="icon-facebook"></span></a>
 
-  <span class="date"><?php the_time('M j, Y'); ?></span>
-	<?php the_tags(); ?>
-	<?php the_category();?>
 </div>
