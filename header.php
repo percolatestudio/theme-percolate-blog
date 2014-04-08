4038e37aff333c9df331ef75df0423f64ed00581
Dominic <dom@percolatestudio.com>
2014-04-08 11:47:32 -0700

    Global: menu & messaging

--- header.php
+++ header.php
@@ -65,7 +65,7 @@
 </head>
 <body <?php body_class($class); ?> lang="en">
 
-  <a class="menu-toggle"><span class="icon-percolate-full"></span></a>
+  <a class="menu-toggle site-nav"><span class="icon-percolate-full"></span></a>
   <?php include('snip_menu.php'); ?>
 
   <div class="content-overlay"><!-- invisible screen to intercept clicks when menu is open --></div>
--- snip_menu.php
+++ snip_menu.php
@@ -1,10 +1,16 @@
 <nav>
-  <div class="wrapper list-vertical">
-    <a href="<?php echo site_url(); ?>">Home</a>
-    <?php
-      $listcats = wp_list_categories('title_li=&style=none&echo=0&orderby=name');
-    	$listcats = str_replace("<br />","",$listcats);
-    	echo $listcats;
-  	 ?>
+  <div class="wrapper">
+    <div class="menu-header">
+      <a class="menu-toggle"><span class="icon-percolate-full"></span></a>
+      <div class="menu-subtitle">A journal about product design & engineering by <a href="http://percolatestudio.com" class="inverse">Percolate Studio</a></div>
+    </div>
+    <div class="list-vertical">
+      <a href="<?php echo site_url(); ?>">Home</a>
+      <?php
+        $listcats = wp_list_categories('title_li=&style=none&echo=0&orderby=name');
+      	$listcats = str_replace("<br />","",$listcats);
+      	echo $listcats;
+    	 ?>
+    </div>
   </div>
 </nav>
