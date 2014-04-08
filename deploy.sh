63e8d992cfc0ca7505f3622d6337814db6df233f
Zoltan Olah <zol@percolatestudio.com>
2014-04-08 11:51:20 -0700

    updated deploy script to blog.etc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh -i ~/.ssh/percolate-oregon.pem -o ForwardAgent=yes ubuntu@newblog.percolatestudio.com "cd ~/theme-percolate-blog; git pull"
+ssh -i ~/.ssh/percolate-oregon.pem -o ForwardAgent=yes ubuntu@blog.percolatestudio.com "cd ~/theme-percolate-blog; git pull"
 
