d76f9a5eed15123940babadafa2e96c5b4d6cb63
Dominic <dom@percolatestudio.com>
2014-05-25 21:52:31 -0700

    Gfycat: restyle play button

--- js/gfycat.js
+++
@@ -1,6 +0,0 @@
-(function(d, t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
-    s.parentNode.insertBefore(g, s);
-}(document, 'script'));
