9de376fa6a5e9cd8b617bc6f2827342f2ce5d7a0
Dominic <dom@percolatestudio.com>
2015-01-25 17:31:49 -0800

    Grid: Remove attribution
    Unnecessary on the archive page as it’s presented as ‘studio’ work.
    Attribution remains on single page

--- snip_post_grid.php
+++ snip_post_grid.php
@@ -16,29 +16,5 @@
         <?php $excerpt = get_the_excerpt(); echo string_limit_words($excerpt,10); ?>
       <?php } ?>
     </h4>
-    <div class="attribution">By
-      <?php
-        $coauthors = get_coauthors($post->ID);
-
-        foreach ($coauthors as $index => $coauthor) :
-          $author_posts_url = get_author_posts_url( $coauthor->ID, $coauthor->user_nicename );
-      ?>
-        <a class="author inverse" href="<?php echo $author_posts_url; ?>"><?php echo $coauthor->display_name; ?></a><span><?php
-            $coauthorsLength = count($coauthors);
-            if($index < $coauthorsLength - 1) {
-              // draw a comma or and
-              if($index == $coauthorsLength - 2) {
-                if ($coauthorsLength == 2) {
-                  echo " &amp; ";
-                } else {
-                  echo " and ";
-                }
-              } else {
-                echo ", ";
-              }
-            }
-          ?></span>
-      <?php endforeach; ?>
-    </div>
   </div>
 </div>
