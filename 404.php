dfc0fafe32c83edd19e7f3628da26ba993e72ec5
Dominic <dom@percolatestudio.com>
2015-02-26 15:51:18 -0800

    404: style the page

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
-
-
-  <header class="default">
-    <h1 class="page-title">Page not found</h1>
-    <h2 class="page-subtitle">Checkout our latest posts instead</h2>
+  <header class="collage short">
+    <div class="grid-2 square">
+      <h1 class="title-page">Not Found</h1>
+      <h2 class="description-page">The blog content you were looking for cannot be found. Checkout our latest posts instead.</h2>
+    </div>
   </header>
+
 
   <div class="collage">
     <?php
